55f2d275927774da8df19c714c971fb5cb33d5c9
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-07-04 02:54:49 +0900

    COMPONENT화 / NEW 페이지 구현(2)
    -일기 본문 구현
    -취소버튼, 작성완료 버튼
    -APP의 DiaryDispatchContext 안에 있는 onCreate에 각각의 데이터를 보내주는 함수 작성

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,12 +1,13 @@
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useRef, useState } from "react";
 
 //COMPONENT
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
+import { DiaryDispatchContext } from "../App";
 
 const Wrapper = styled.div``;
 
@@ -39,6 +40,26 @@ const EmotionWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(5, auto);
   gap: 2%;
+`;
+
+const TextArea = styled.textarea`
+  font-family: "Nanum Pen Script";
+  font-size: 20px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 200px;
+  resize: vertical; //높이만 조절가능
+  border: none;
+  border-radius: 5px;
+  background-color: #ececec;
+  padding: 20px;
+  outline: none;
+`;
+
+const ControlBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 //styled ---------------------------------------------------------------
@@ -81,17 +102,37 @@ ex)2023-07-03T10:14:58.123Z 같이 나온다 그래서 slice를 해주어 자르
 function DiaryEditor() {
   const navigate = useNavigate();
   const [date, setDate] = useState(getStringDate(new Date()));
-  const [emotion, setEmotion] = useState();
+  const [emotion, setEmotion] = useState(3);
+  const contentRef = useRef();
+  const [content, setContent] = useState("");
 
-  const onChange = (e) => {
+  const onChangeDate = (e) => {
     setDate(e.target.value);
   };
-  //날짜 input 함수
+  //날짜 input onChange 함수
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
   };
   // 이모션 클릭 함수
+
+  const onChangeText = (e) => {
+    setContent(e.target.value);
+  };
+  // 본문 text onChange 함수
+
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus(); //length가 1보다 작으면 저장안되고 포커스
+      return;
+    }
+
+    onCreate(date, content, emotion); //onCreate에 각각의 데이터를 보내줌
+    navigate(`/`, { replace: true }); //replace:true를 하면 페이지 뒤로가기를 눌러도 새일기쓰기 페이지로 다시넘어오지 않는다
+  };
+  // 작성완료 하면 APP DiaryDispatchContext의 onCreate에 각각의 데이터를 보내주는 함수
 
   return (
     <Wrapper>
@@ -106,9 +147,10 @@ function DiaryEditor() {
         <Section>
           <h4>오늘은 언제인가요?</h4>
           <DateDiv>
-            <DateInput type="date" value={date} onChange={onChange} />
+            <DateInput type="date" value={date} onChange={onChangeDate} />
           </DateDiv>
         </Section>
+        {/* 날짜 섹션 */}
 
         <Section>
           <h4>오늘의 감정</h4>
@@ -124,6 +166,30 @@ function DiaryEditor() {
             ))}
           </EmotionWrapper>
         </Section>
+        {/* 감정 섹션 */}
+
+        <Section>
+          <h4>오늘의 일기</h4>
+          <TextArea
+            placeholder="오늘은 어땠나요?"
+            ref={contentRef}
+            value={content}
+            onChange={onChangeText}
+          />
+        </Section>
+        {/* 일기 본문 섹션 */}
+
+        <Section>
+          <ControlBox>
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
+            <MyButton
+              text={"작성완료"}
+              type="positive"
+              onClick={handleSubmit}
+            />
+          </ControlBox>
+        </Section>
+        {/*컨트롤박스 섹션  */}
       </SectionWrapper>
     </Wrapper>
   );
